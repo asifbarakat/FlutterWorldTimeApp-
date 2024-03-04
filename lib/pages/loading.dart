8c08467ed657b8a71c4ebcbc449e6e015036a3f2
asifbarakat <121655875+asifbarakat@users.noreply.github.com>
2024-03-04 17:34:23 +0600

    Add files via upload

---
+++ lib/pages/loading.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class Loading extends StatefulWidget {
+  const Loading({super.key});
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void setupWorldTime() async {
+    WorldTime instance =
+        WorldTime(location: 'Dhaka', flag: 'bangladesh.png', url: 'Asia/Dhaka');
+    await instance.getTime();
+    Future.delayed(Duration(seconds: 4), () {
+      Navigator.pushReplacementNamed(context, '/home', arguments: {
+        'Location': instance.location,
+        'Flag': instance.flag,
+        'Time': instance.time,
+        'isDayTime': instance.isDayTime ? 'Day' : 'Night',
+      });
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setupWorldTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[900],
+      body: Center(
+        child: SpinKitPouringHourGlass(
+          color: Colors.blue,
+          size: 80.0,
+        ),
+      ),
+    );
+  }
+}
